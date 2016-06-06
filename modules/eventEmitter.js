5643852db5b806388f4f1badb6bc997fa49af686
Jack Palmer <jackpalmer2@gmail.com>
2016-06-06 14:18:06 +0100

    Adding event emitter

---
+++ modules/eventEmitter.js
@@ -0,0 +1,46 @@
+(function() {
+
+  'use strict';
+
+  function EventEmitter() {
+    this.init.apply(this, arguments);
+  }
+
+  EventEmitter.prototype.init = function() {
+    this.callbacks = {};
+  };
+
+  /* Add new callback to an event */
+  EventEmitter.prototype.on = function(event, callback) {
+    if(!this.callbacks[event]) { this.callbacks[event] = []; }
+
+    this.callbacks[event].push(callback);
+    return this.callbacks[event];
+  };
+
+  /* Remove all callbacks for event */
+  EventEmitter.prototype.off = function(event) {
+    this.callbacks[event] = [];
+    return this.callbacks[event];
+  };
+
+  /* Unbinds a single callback on an event */
+  EventEmitter.prototype.unbind = function(event, callback) {
+    this.callbacks[event].splice(this.callbacks[event].indexOf(callback), 1);
+    return this.callbacks[event];
+  };
+
+  /* Emits an event calling all callbacks */
+  EventEmitter.prototype.emit = function(event) {
+    var args = Array.prototype.slice.call(this, arguments);
+
+    if(this.callbacks[event] && this.callbacks[event].length) {
+      this.callbacks[event].forEach(function loopCallbacks(fn) {
+        fn.apply(this, args);
+      });
+    }
+  };
+
+  return EventEmitter;
+
+})();
